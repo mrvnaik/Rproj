2db5200191f708f2e35688c31615cdfd382a6bd1
Vasudeva Naik <mr.vnaik@gmail.com>
2015-02-18 18:19:19 +0800

    1st commit
    1st commit

---
+++ Scripts/towerOfHoney.R
@@ -0,0 +1,25 @@
+
+println<-function(format,...){
+  x<-sprintf(format,...)
+  x<-sprintf("%s\n",x)
+  cat(x)  
+}
+if(FALSE) {
+  println("x=%s", 10)
+}
+
+towerOfHoney<-function(n, src, dest, temp)
+{
+  if(n==1){
+    println("%s from %s to %s***", n, src, dest)  
+    return();
+    print("***####***")
+  }
+  towerOfHoney(n-1, src, temp, dest)
+  println("%s from %s to %s", n, src, dest)  
+  towerOfHoney(n-1, temp, dest, src)
+  
+}
+if(FALSE){
+  towerOfHoney(5, "A", "B", "C")
+}
